a0d1b711efbb50d812479e0e9517a67ebf053e53
BisiDavi <oludavidconnect@gmail.com>
2022-08-12 18:37:45 +0100

    button updated

--- components/BannerTypewriter.tsx
+++ components/BannerTypewriter.tsx
@@ -39,7 +39,7 @@ export default function BannerTypewriter() {
           .circles {
             position: absolute;
             top: 0;
-            left: 0;
+            left: 0;j
             width: 100%;
             height: 100%;
             overflow: hidden;
