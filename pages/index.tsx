b9eb512c1b2b2f0720f2ce191c65e96232d5d5ca
BisiDavi <oludavidconnect@gmail.com>
2022-08-14 15:19:58 +0100

    service page updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 
 import HomepageBanner from "@/components/HomepageBanner";
--- pages/service/[slug].tsx
+++ pages/service/[slug].tsx
@@ -1,18 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { useAppSelector } from "@/hooks/useRedux";
 import DefaultLayout from "@/layout/Default-layout";
 import formatBigInt from "@/lib/formatBigInt";
 import squareClient from "@/lib/squareClient";
-import { serviceItemType } from "@/types/service-type";
+import { useAppDispatch } from "@/redux/store";
+import { updateStoreProfile } from "@/redux/store-profile-slice";
+
 import type { GetServerSideProps } from "next";
+import type { serviceItemType } from "@/types/service-type";
+import type { storeProfileType } from "@/types/store-types";
 
 interface Props {
   service: serviceItemType;
+  storeProfile: storeProfileType;
 }
 
-export default function ServicePage({ service }: Props) {
-  console.log("service", service);
+export default function ServicePage({
+  service,
+  storeProfile: storeProfileData,
+}: Props) {
+  const dispatch = useAppDispatch();
+  const { storeProfile } = useAppSelector((state) => state.StoreProfile);
+
+  useEffect(() => {
+    if (storeProfile === null) {
+      dispatch(updateStoreProfile(storeProfileData));
+    }
+  }, []);
+
   return (
     <DefaultLayout>
-      <section className="service-page">
+      <section className="service-page container mx-auto">
         <h1 className="font-bold text-xl">{service.itemData.name}</h1>
       </section>
     </DefaultLayout>
@@ -24,6 +43,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const serviceId: string | any = context.query.id;
   try {
     const response = await client.catalogApi.retrieveCatalogObject(serviceId);
+    const storeProfileData = await client.locationsApi.listLocations();
 
     console.log(response.result);
     const serviceResult = formatBigInt(response.result);
@@ -32,6 +52,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {
       props: {
         service: parsedServiceResult.object,
+        storeProfile: storeProfileData.result.locations
+          ? storeProfileData.result.locations[0]
+          : null,
       },
     };
   } catch (error) {
