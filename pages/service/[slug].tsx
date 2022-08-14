677ada1ce665b6282bb09404ff4a8c86255954b1
BisiDavi <oludavidconnect@gmail.com>
2022-08-14 19:39:36 +0100

    servicepage sidebar added.

--- pages/service/[slug].tsx
+++ pages/service/[slug].tsx
@@ -1,18 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
+
 import { useAppSelector } from "@/hooks/useRedux";
 import DefaultLayout from "@/layout/Default-layout";
 import formatBigInt from "@/lib/formatBigInt";
 import squareClient from "@/lib/squareClient";
 import { useAppDispatch } from "@/redux/store";
 import { updateStoreProfile } from "@/redux/store-profile-slice";
+import ServicePageSidebar from "@/components/Sidebar/ServicePageSidebar";
 
 import type { GetServerSideProps } from "next";
 import type { serviceItemType } from "@/types/service-type";
 import type { storeProfileType } from "@/types/store-types";
-import { BsPhone } from "react-icons/bs";
-import formatTime from "@/lib/formatTime";
-import formatDays from "@/lib/formatDays";
 
 interface Props {
   service: serviceItemType;
@@ -38,42 +37,7 @@ export default function ServicePage({
         <h1 className="font-bold text-xl">{service.itemData.name}</h1>
         <div className="content h-screen flex">
           <div className="w-3/4 bg-white"></div>
-          <div className="w-1/4 bg-gray-200 rounded-lg px-4 py-2">
-            <h6 className="text-md pt-2 text-gray-600 font-bold">ABOUT US</h6>
-            <p className="pt-1 pb-8">{service.itemData.description}</p>
-            <h6 className="text-md py-1  text-gray-600 font-bold">
-              CONTACT & BUSINESS HOURS
-            </h6>
-            <hr className="w-full bg-gray-400 h-1" />
-            <div className="call flex items-center justify-between py-4">
-              <div className="flex items-center">
-                <BsPhone className="mr-2" /> <p>{storeProfile?.phoneNumber}</p>
-              </div>
-              <div className="dial-phone font-medium text-center bg-white border border-gray-100 rounded w-1/6 hover:bg-gray-100">
-                <a
-                  className="text-md"
-                  href={`tel:+${storeProfile?.phoneNumber}`}
-                >
-                  Call
-                </a>
-              </div>
-            </div>
-            <hr className="w-full bg-gray-400 h-1" />
-            <ul className="opening-days mt-4">
-              {storeProfile?.businessHours.periods.map((period) => (
-                <li
-                  key={period.dayOfWeek}
-                  className="text-sm flex justify-between py-1"
-                >
-                  {formatDays(period.dayOfWeek)}{" "}
-                  <span>
-                    {period.startLocalTime} AM -{" "}
-                    {formatTime(period.endLocalTime)} PM{" "}
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <ServicePageSidebar service={service} />
         </div>
       </section>
     </DefaultLayout>
