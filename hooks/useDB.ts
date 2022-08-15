b63a1ea4a79c782b0cd3c7e32d01623be7b873e1
BisiDavi <oludavidconnect@gmail.com>
2022-08-15 18:57:51 +0100

    DB hook updated

--- hooks/useDB.ts
+++ hooks/useDB.ts
@@ -2,14 +2,31 @@ import { initializeApp } from "firebase/app";
 
 export default async function useDB() {
   const firebaseConfig = {
-    databaseURL:process.env.NEXT_PUBLIC_FIREBASE_DB_URL
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
   };
 
   async function initializeDB() {
     const app = initializeApp(firebaseConfig);
-    const db = await import("firebase/database");
-    const database = db.getDatabase(app);
-    return database;
+    const { getDatabase, ref, set, onValue } = await import(
+      "firebase/database"
+    );
+    const db = getDatabase(app);
+    return { db, ref, set, onValue };
   }
-  return { initializeDB };
+
+  async function writeData(data: any, dbNode: string) {
+    const { db, ref, set } = await initializeDB();
+    set(ref(db, dbNode), data);
+  }
+
+  async function readData(dbNode: string) {
+    const { db, ref, onValue } = await initializeDB();
+    const dataRef = ref(db, dbNode);
+    onValue(dataRef, (snapshot) => {
+      const data = snapshot.val();
+      return data;
+    });
+  }
+
+  return { initializeDB, writeData, readData };
 }
--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
+
 import RootReducer from "@/redux/rootReducer";
 
 const store = configureStore({
