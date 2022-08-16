ba1d2ca9e7f65ff7e76d1bb59a679387130eec69
BisiDavi <oludavidconnect@gmail.com>
2022-08-16 05:10:19 +0100

    booking page updated

--- pages/book/[slug].tsx
+++ pages/book/[slug].tsx
@@ -1,7 +1,30 @@
-import BookCalendar from "@/components/Calendar/BookCalendar";
-import DefaultLayout from "@/layout/Default-layout";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
 
-export default function BookPage() {
+import { useAppSelector } from "@/hooks/useRedux";
+import DefaultLayout from "@/layout/Default-layout";
+import squareClient from "@/lib/squareClient";
+import { useAppDispatch } from "@/redux/store";
+import BookCalendar from "@/components/Calendar/BookCalendar";
+import { updateStoreProfile } from "@/redux/store-profile-slice";
+
+import type { GetServerSideProps } from "next";
+import type { storeProfileType } from "@/types/store-types";
+
+interface Props {
+  storeProfile: storeProfileType;
+}
+
+export default function BookPage({ storeProfile: storeProfileData }: Props) {
+  const dispatch = useAppDispatch();
+  const { storeProfile } = useAppSelector((state) => state.StoreProfile);
+
+  useEffect(() => {
+    if (storeProfile === null) {
+      dispatch(updateStoreProfile(storeProfileData));
+    }
+  }, []);
+
   return (
     <DefaultLayout>
       <div className="content container flex items-center mx-auto py-4">
@@ -13,3 +36,24 @@ export default function BookPage() {
     </DefaultLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { client } = await squareClient();
+  try {
+    const storeProfileData = await client.locationsApi.listLocations();
+
+    return {
+      props: {
+        storeProfile: storeProfileData.result.locations
+          ? storeProfileData.result.locations[0]
+          : null,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        storeProfile: null,
+      },
+    };
+  }
+};
--- pages/service/[slug].tsx
+++ pages/service/[slug].tsx
@@ -31,7 +31,7 @@ export default function ServicePage({
       dispatch(updateStoreProfile(storeProfileData));
     }
   }, []);
- 
+
   return (
     <DefaultLayout>
       <main className="service-page container mx-auto pt-20">
@@ -67,6 +67,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {
       props: {
         service: null,
+        storeProfileData: null,
       },
     };
   }
