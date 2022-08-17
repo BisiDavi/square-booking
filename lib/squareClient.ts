70379b4c1d31a056466ec68b2e0060d302b87200
BisiDavi <oludavidconnect@gmail.com>
2022-08-17 06:58:06 +0100

    obtain token api updated

--- lib/squareClient.ts
+++ lib/squareClient.ts
@@ -1,10 +1,14 @@
-export default async function squareClient() {
+export default async function squareClient(environment?: string) {
   const squareClient = await import("square");
+
+  const squareEnvironment = environment
+    ? squareClient.Environment.Production
+    : squareClient.Environment.Sandbox;
+
   const client = new squareClient.Client({
     accessToken: process.env.NEXT_PUBLIC_SQUARE_SANDBOX_ACCESS_TOKEN,
-    environment: squareClient.Environment.Sandbox,
+    environment: squareEnvironment,
   });
 
   return { client };
 }
- 
--- pages/api/oauth/obtain-access-token.ts
+++ pages/api/oauth/obtain-access-token.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import squareClient from "@/lib/squareClient";
+import axios from "axios";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function Handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { client } = await squareClient();
-
   const { squareCode } = req.body;
   const clientId = `${process.env.NEXT_PUBLIC_SQUARE_PRODUCTION_APP_ID}`;
   const clientSecret = `${process.env.NEXT_PUBLIC_SQUARE_PRODUCTION_ACCESS_TOKEN}`;
@@ -15,21 +14,28 @@ export default async function Handler(
   switch (req.method) {
     case "POST": {
       try {
-        const response = await client.oAuthApi.obtainToken({
-          clientId,
-          clientSecret,
-          grantType: "authorization_code",
-          code: squareCode,
-          shortLived: false,
-          scopes: [
-            "APPOINTMENTS_READ",
-            "APPOINTMENTS_WRITE",
-            "APPOINTMENTS_ALL_READ",
-            "APPOINTMENTS_BUSINESS_SETTINGS_READ",
-          ],
-        });
-        console.log("retrieveBusinessBookingProfile", response.result);
-        res.status(200).json(response.result);
+        axios
+          .post("https://connect.squareup.com/oauth2/token", {
+            clientId,
+            clientSecret,
+            grantType: "authorization_code",
+            code: squareCode,
+            shortLived: false,
+            scopes: [
+              "APPOINTMENTS_READ",
+              "APPOINTMENTS_WRITE",
+              "APPOINTMENTS_ALL_READ",
+              "APPOINTMENTS_BUSINESS_SETTINGS_READ",
+            ],
+          })
+          .then((response) => {
+            console.log("obtain-access-token-response", response);
+            res.status(200).json(response);
+          })
+          .catch((error) => {
+            console.log("obtain-access-token-error", error);
+            res.status(400).json(error);
+          });
       } catch (error) {
         console.log("error", error);
         res.status(400).json(error);
