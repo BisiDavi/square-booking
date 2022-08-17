7087fdca367d4beb2faefdf6575f15eedc81b4da
BisiDavi <oludavidconnect@gmail.com>
2022-08-17 13:06:00 +0100

    obtain access token updated

--- pages/api/oauth/obtain-access-token.ts
+++ pages/api/oauth/obtain-access-token.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios";
+
 import { DBClient } from "@/DB/DBConnection";
 import saveAccessTokenToDB from "@/DB/saveAccessTokenToDB";
 
@@ -14,23 +15,28 @@ export default async function Handler(
   const client_secret = `${process.env.NEXT_PUBLIC_SQUARE_PRODUCTION_ACCESS_TOKEN}`;
   const dbClient = await DBClient();
 
+  const postData = {
+    client_id,
+    client_secret,
+    grant_type: "authorization_code",
+    code: squareCode,
+    short_lived: false,
+    scopes: [
+      "APPOINTMENTS_READ",
+      "APPOINTMENTS_WRITE",
+      "APPOINTMENTS_ALL_READ",
+      "APPOINTMENTS_BUSINESS_SETTINGS_READ",
+    ],
+  };
+
   switch (req.method) {
     case "POST": {
       try {
         axios
-          .post("https://connect.squareup.com/oauth2/token", {
-            client_id,
-            client_secret,
-            grant_type: "authorization_code",
-            code: squareCode,
-            short_lived: false,
-            scopes: [
-              "APPOINTMENTS_READ",
-              "APPOINTMENTS_WRITE",
-              "APPOINTMENTS_ALL_READ",
-              "APPOINTMENTS_BUSINESS_SETTINGS_READ",
-            ],
-          })
+          .post(
+            "https://connect.squareup.com/oauth2/token",
+            JSON.stringify(postData)
+          )
           .then((response) => {
             console.log("obtain-access-token-response", response);
             saveAccessTokenToDB(dbClient, response);
