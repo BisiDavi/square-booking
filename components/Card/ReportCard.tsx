cdd159fdc402238cd5c6b3df72e68da3eec6da0f
BisiDavi <oludavidconnect@gmail.com>
2022-08-18 12:19:11 +0100

    icons added

--- components/Card/ReportCard.tsx
+++ components/Card/ReportCard.tsx
@@ -1,19 +1,25 @@
-import { BsRecordCircleFill } from "react-icons/bs";
-
+/* eslint-disable @next/next/no-img-element */
 interface Props {
   report: {
     count: number;
     text: string;
+    icon: string;
   };
 }
 
 export default function ReportCard({ report }: Props) {
   return (
-    <div className="rounded-lg shadow bg-white w-1/4 mr-3 p-4 text-xl flex items-center">
-      <BsRecordCircleFill className="text-3xl mr-4" />
+    <div className="rounded-lg shadow bg-white w-1/4 mr-3 p-4 py-6 hover:bg-gray-50 justify-center text-xl flex items-center">
+      <img
+        src={report.icon}
+        alt={report.text}
+        height={50}
+        width={50}
+        className="text-3xl mr-4"
+      />
       <div className="text text-center flex flex-col justify-center">
-        <h3>{report.count}</h3>
-        <p className="text-sm text-gray-500 font-bold text-center">
+        <h3 className="font-bold">{report.count}</h3>
+        <p className="text-sm text-gray-400 font-bold text-center">
           {report.text}
         </p>
       </div>
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,19 +2,22 @@ import ReportCard from "@/components/Card/ReportCard";
 import AdminLayoutPage from "@/layout/Admin-layout";
 
 const activityOverview = [
-  { text: "Total Bookings", count: 0 },
-  { text: "Total Customers", count: 0 },
-  { text: "Total Services", count: 0 },
-  { text: "Staff Total ", count: 0 },
+  { text: "Total Bookings", count: 0, icon: "/bookingIcon.png" },
+  { text: "Total Customers", count: 0, icon: "/customer.png" },
+  { text: "Total Services", count: 0, icon: "/services.png" },
+  { text: "Staff Total ", count: 0, icon: "staff.png" },
 ];
 
 export default function Dashboard() {
   return (
     <AdminLayoutPage>
-      <div className="overview flex items-center mt-20">
+      <div className="overview flex items-center mt-10">
         {activityOverview.map((item, index) => (
           <ReportCard key={`${item}-${index}`} report={item} />
         ))}
+      </div>
+      <div className="quick-task mt-4">
+        <h3 className="font-medium">Quick Task</h3>
       </div>
     </AdminLayoutPage>
   );
