2c97633ef915271abd5ac411f17b6a61372eb784
BisiDavi <oludavidconnect@gmail.com>
2022-08-18 04:06:21 +0100

    dashboard updated

--- components/Sidebar/DashboardSidebar.tsx
+++ components/Sidebar/DashboardSidebar.tsx
@@ -3,21 +3,36 @@ import Link from "next/link";
 import sidebarContent from "@/json/dashboard-sidebar.json";
 import SidebarIcons from "@/icons/SidebarIcons";
 
+interface Props {
+  linkGroup: "links" | "extras";
+}
+
+function SidebarLinks({ linkGroup }: Props) {
+  return (
+    <ul>
+      {sidebarContent[linkGroup].map((content) => (
+        <li
+          className="my-4 hover:bg-gray-100 p-2 px-6 hover:text-purple-500"
+          key={content.link}
+        >
+          <Link href={content.link} passHref>
+            <a className="flex items-center">
+              <SidebarIcons icon={content.text} />
+              <span className="ml-2 text-xl">{content.text}</span>
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default function DashboardSidebar() {
   return (
-    <aside className="w-1/4 h-screen border border-r fixed top-32">
-      <ul className="link-list">
-        {sidebarContent.map((content) => (
-          <li className="my-4 hover:bg-gray-100 p-2 px-6 hover:text-purple-500" key={content.link}>
-            <Link href={content.link} passHref>
-              <a className="flex items-center">
-                <SidebarIcons icon={content.text} />
-                <span className="ml-2 text-xl">{content.text}</span>
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <aside className="w-1/4 h-screen border border-r fixed top-20">
+      <SidebarLinks linkGroup="links" />
+      <hr className="mb-10 mt-2" />
+      <SidebarLinks linkGroup="extras" />
     </aside>
   );
 }
--- icons/SidebarIcons.tsx
+++ icons/SidebarIcons.tsx
@@ -1,15 +1,19 @@
 import { AiOutlineHome } from "react-icons/ai";
-import { BsFillPersonLinesFill, BsPersonBadge } from "react-icons/bs";
-import { GrServices } from "react-icons/gr";
+import {
+  BsFillPersonLinesFill,
+  BsInstagram,
+  BsPersonBadge,
+} from "react-icons/bs";
 import { RiTeamLine } from "react-icons/ri";
 import { FiSettings } from "react-icons/fi";
+import { MdMiscellaneousServices } from "react-icons/md";
 
 function displaySidebarIcons(icon: string) {
   switch (icon) {
     case "Home":
       return <AiOutlineHome />;
     case "Services":
-      return <GrServices />;
+      return <MdMiscellaneousServices />;
     case "Customers":
       return <BsFillPersonLinesFill />;
     case "Team":
@@ -18,6 +22,8 @@ function displaySidebarIcons(icon: string) {
       return <BsPersonBadge />;
     case "Account & Settings":
       return <FiSettings />;
+    case "Link with Instagram":
+      return <BsInstagram />;
   }
 }
 
