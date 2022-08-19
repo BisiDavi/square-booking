5f9b1bb3eb373c489e5cd67b95da4bd8e872cf71
BisiDavi <oludavidconnect@gmail.com>
2022-08-19 05:09:30 +0100

    auth modal updated

--- components/Modal/AuthModal.tsx
+++ components/Modal/AuthModal.tsx
@@ -1,20 +1,12 @@
-import Link from "next/link";
 import { memo } from "react";
 
 import Modal from "@/components/Modal";
 import Tabs from "@/components/Tab";
 import AuthForm from "@/components/Form/AuthForm";
 import useUI from "@/hooks/useUI";
-import { useRouter } from "next/router";
 
 function AuthModalComponent() {
   const { modal, toggleModal } = useUI();
-  const router = useRouter();
-
-  function closeModal() {
-    router.push("/admin/login");
-    toggleModal(null);
-  }
 
   return (
     <Modal
@@ -27,18 +19,6 @@ function AuthModalComponent() {
         tabBody1={<AuthForm type="signin" />}
         tabBody2={<AuthForm type="signup" />}
       />
-      <h3 className="text-center font-medium text-xl">
-        To become a Seller,
-        <Link href="/admin/login" passHref>
-          <button
-            className="text-blue-500 hover:text-red-500 ml-1"
-            type="button"
-            onClick={closeModal}
-          >
-            Login / Signup{" "}
-          </button>
-        </Link>
-      </h3>
     </Modal>
   );
 }
