a20cb91eb1190e7b8b8de837bfa42b0d01d5aa72
BisiDavi <oludavidconnect@gmail.com>
2022-08-21 16:59:41 +0100

    report card updated

--- components/Card/ReportCard.tsx
+++ components/Card/ReportCard.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import reportCardQueryFunction, {
   parseReportCard,
 } from "@/components/Card/reportCardQueryFunction";
+import Link from "next/link";
 
 /* eslint-disable @next/next/no-img-element */
 interface Props {
@@ -11,6 +12,7 @@ interface Props {
     icon: string;
     method: string;
     key: string;
+    link: string;
   };
 }
 
@@ -26,20 +28,22 @@ export default function ReportCard({ report }: Props) {
   const count = parsedData ? parsedData.length : 0;
 
   return (
-    <div className="rounded-lg shadow bg-white w-1/4 mr-3 p-4 py-6 hover:bg-gray-50 justify-center text-xl flex items-center">
-      <img
-        src={report.icon}
-        alt={report.text}
-        height={50}
-        width={50}
-        className="text-3xl mr-4"
-      />
-      <div className="text text-center flex flex-col justify-center">
-        {parsedData && <h3 className="font-bold">{count}</h3>}
-        <p className="text-sm text-gray-400 font-bold text-center">
-          {report.text}
-        </p>
-      </div>
-    </div>
+    <Link href={report.link} passHref>
+      <a className="rounded-lg shadow bg-white w-1/4 mr-3 p-4 py-6 hover:bg-gray-50 justify-center text-xl flex items-center">
+        <img
+          src={report.icon}
+          alt={report.text}
+          height={50}
+          width={50}
+          className="text-3xl mr-4"
+        />
+        <div className="text text-center flex flex-col justify-center">
+          {parsedData && <h3 className="font-bold">{count}</h3>}
+          <p className="text-sm text-gray-400 font-bold text-center">
+            {report.text}
+          </p>
+        </div>
+      </a>
+    </Link>
   );
 }
