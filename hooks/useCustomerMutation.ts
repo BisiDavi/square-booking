98d2016d14109cb74018aa1b8027ff2db6890406
BisiDavi <oludavidconnect@gmail.com>
2022-08-21 14:23:38 +0100

    firebase removed

--- hooks/useCustomerMutation.ts
+++ hooks/useCustomerMutation.ts
@@ -1,6 +1,5 @@
 import { createCustomer } from "@/requests/postRequests";
 import useRequestMutation from "@/hooks/useRequestMutation";
-import useFirebase from "@/hooks/useFirebase";
 
 import type { createCustomerType } from "@/types/request-types";
 
@@ -12,32 +11,11 @@ function createSquareCustomer({
   return createCustomer({ email, lastName, firstName });
 }
 
-function saveCustomerToDb(writeData: any, getAuthdetails: any, data: any) {
-  const authDetails = getAuthdetails();
-  const uid = authDetails.uid;
-  if (typeof data === "object") {
-    const stringifyData = JSON.stringify(data);
-    writeData(
-      stringifyData,
-      `/users/${uid}/square/customer/${data.customer.id}`
-    );
-  } else if (typeof data === "string") {
-    const responseData: any = JSON.parse(data);
-    writeData(
-      data,
-      `/users/${uid}/square/customer/${responseData.customer.id}`
-    );
-  }
-}
-
 export default function useCustomerMutation() {
-  const { writeData, getAuthdetails } = useFirebase();
   function useCreateSquareCustomer() {
     return useRequestMutation(createSquareCustomer, {
       mutationKey: "useCreateSquareCustomer",
       success: "Customer created Successfully",
-      onSuccessMethodWithData: (data: any) =>
-        saveCustomerToDb(writeData, getAuthdetails, data),
       error: "Error creating customer",
     });
   }
