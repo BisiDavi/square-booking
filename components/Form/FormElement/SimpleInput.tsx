3419bcb0a0a59cfaba11055f6ea4336d4197b2f5
BisiDavi <oludavidconnect@gmail.com>
2022-08-22 00:21:14 +0100

    input updated

--- components/Form/FormElement/SimpleInput.tsx
+++ components/Form/FormElement/SimpleInput.tsx
@@ -1,25 +1,28 @@
-import toSlug from "@/lib/toSlug";
+import useReduxForm from "@/hooks/useReduxForm";
 
 interface Props {
   input: {
     name: string;
     type?: string;
     placeholder: string;
+    formType: string;
   };
 }
 
 export default function SimpleInput({ input }: Props) {
-  const { name, type, placeholder } = input;
-  const id = toSlug(name);
+  const { name, type, placeholder, formType } = input;
   const inputType = type ? type : "text";
+  const { getInputValue, onChangeHandler } = useReduxForm();
+  const id = getInputValue(`${name}-${formType}`);
   return (
     <div className="simple input my-3">
       <input
         type={inputType}
         id={id}
-        name={name}
+        name={id}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 focus:border-1 block w-full p-2.5  placeholder-gray-400 text-white"
         placeholder={placeholder}
+        onChange={(e) => onChangeHandler(e, id)}
       />
     </div>
   );
--- components/Form/FormElement/displayFormElement.tsx
+++ components/Form/FormElement/displayFormElement.tsx
@@ -1,4 +1,4 @@
-import TextArea from "@/components/Form/FormElement/TextAreaElement";
+import TextArea from "@/components/Form/FormElement/TextArea";
 import Input from "@/components/Form/FormElement/Input";
 import CategoryDropdown from "@/components/Form/FormElement/CategoryDropdown";
 import FormModalGroup from "@/components/Form/FormElement/FormModalGroup";
