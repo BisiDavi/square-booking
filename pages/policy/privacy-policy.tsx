2630a1cf2dee5a0bb741e5e4a65f76263c12e52e
BisiDavi <oludavidconnect@gmail.com>
2022-08-23 11:48:26 +0100

    policy page added

---
+++ pages/policy/privacy-policy.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from "@/layout/Default-layout";
+
+export default function PrivacyPolicy() {
+  return (
+    <DefaultLayout>
+      <h3>privacy-policy page</h3>
+    </DefaultLayout>
+  );
+}
---
+++ pages/policy/terms-and-condition.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from "@/layout/Default-layout";
+
+export default function TermsAndConditionPage() {
+  return (
+    <DefaultLayout>
+      <h3>Terms and condition page page</h3>
+    </DefaultLayout>
+  );
+}
